0e1704b1e716fb7a35611bb8f772fe67472a6321
aiandgreat <ajsgarcia.student@ua.edu.ph>
2026-06-02 20:19:13 +0800

    revert

--- Certifier_App/utils/google_oauth.py
+++ Certifier_App/utils/google_oauth.py
@@ -29,7 +29,6 @@ def _get_google_oauth_config():
     redirect_uri = _get_oauth_setting(
         'GOOGLE_OAUTH_REDIRECT_URI',
         'http://127.0.0.1:8000/api/auth/google/callback/',
-        'https://certifier-backend-b2ne.onrender.com/api/auth/google/callback/'
         
     )
     return client_id, client_secret, redirect_uri
