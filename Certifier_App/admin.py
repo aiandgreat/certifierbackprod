9c9188033cd0ce4bc8253390096e3cc9712cbefe
aiandgreat <ajsgarcia.student@ua.edu.ph>
2026-06-25 18:45:01 +0800

    Register Department model in django admin and display department field in UserAdmin views

--- Certifier_App/admin.py
+++ Certifier_App/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User, Certificate, Template, BulkUpload
+from .models import User, Certificate, Template, BulkUpload, Department
 
 # ================= USER ADMIN =================
 class UserAdmin(BaseUserAdmin):
@@ -13,6 +13,7 @@ class UserAdmin(BaseUserAdmin):
                 'first_name',
                 'last_name',
                 'role',
+                'department',
             )
         }),
         ('Permissions', {
@@ -38,6 +39,7 @@ class UserAdmin(BaseUserAdmin):
                 'password1',
                 'password2',
                 'role',
+                'department',
                 'is_staff',
                 'is_superuser',
                 'is_active',
@@ -45,7 +47,7 @@ class UserAdmin(BaseUserAdmin):
         }),
     )
 
-    list_display = ('username', 'email', 'role', 'is_staff', 'is_superuser')
+    list_display = ('username', 'email', 'role', 'department', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     ordering = ('username',)
 
@@ -56,6 +58,12 @@ class UserAdmin(BaseUserAdmin):
             obj.is_superuser = True
         super().save_model(request, obj, form, change)
 
+
+# ================= DEPARTMENT ADMIN =================
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'abbreviation')
+    search_fields = ('name', 'abbreviation')
 
 
 # ================= REGISTER MODELS =================
